d62470b521811c7d1b3d6464d7c9138f0fcaf810
Alexander Ryazantsev <shurik.v.r@gmail.com>
2025-01-24 17:48:45 +0300

    Configure TS and Jest

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "@jest/types"
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  testMatch: ['**/*.test.ts'],
+  verbose: true
+}
+
+export default config;
