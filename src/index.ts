18bbb9cab341c1b1dcc1711b578246984f2a81a7
Alexander Ryazantsev <shurik.v.r@gmail.com>
2025-01-30 17:13:59 +0300

    Format code with Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import {execSync} from "node:child_process";
-import {existsSync} from "fs";
+import { execSync } from "node:child_process";
+import { existsSync } from "fs";
 import * as path from "node:path";
 
 type Settings = {
   dumpsLocation?: string;
   excludeTables?: string[];
   database: string;
-}
+};
 
 export class DumpHook {
   public static readonly DEFAULT_SETTINGS = {
@@ -16,14 +16,14 @@ export class DumpHook {
   private readonly settings: Required<Settings>;
 
   constructor(settings: Settings) {
-    this.settings = {...settings, ...DumpHook.DEFAULT_SETTINGS};
+    this.settings = { ...settings, ...DumpHook.DEFAULT_SETTINGS };
   }
 
   async execute(fileName: string, callback: () => Promise<void>) {
-    if(!existsSync(this.settings.dumpsLocation)) await this.createCacheDir();
+    if (!existsSync(this.settings.dumpsLocation)) await this.createCacheDir();
 
     const filePath = path.join(this.settings.dumpsLocation, `${fileName}.dump`);
-    if(existsSync(filePath)) {
+    if (existsSync(filePath)) {
       await this.restore(filePath);
     } else {
       await callback();
@@ -32,18 +32,27 @@ export class DumpHook {
   }
 
   private async createCacheDir() {
-    execSync(`mkdir -p ${this.settings.dumpsLocation}`)
+    execSync(`mkdir -p ${this.settings.dumpsLocation}`);
   }
 
   private async dump(filePath: string) {
-    const args: string[] = ["-d", this.settings.database, "-a", "-x", "-O", "-f", filePath, "-Fc"];
-    for(const table of this.settings.excludeTables) {
-      args.push("-T", table)
+    const args: string[] = [
+      "-d",
+      this.settings.database,
+      "-a",
+      "-x",
+      "-O",
+      "-f",
+      filePath,
+      "-Fc"
+    ];
+    for (const table of this.settings.excludeTables) {
+      args.push("-T", table);
     }
-    execSync(`pg_dump ${args.join(" ")}`)
+    execSync(`pg_dump ${args.join(" ")}`);
   }
 
   private async restore(filePath: string) {
-    execSync(`pg_restore -d ${this.settings.database} ${filePath}`)
+    execSync(`pg_restore -d ${this.settings.database} ${filePath}`);
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,7 @@
-import {DumpHook} from "./index";
-import {execSync} from "node:child_process";
-import {existsSync, rmSync} from "fs";
+import { DumpHook } from "../src";
+import { execSync } from "node:child_process";
+import { existsSync, rmSync } from "fs";
 import postgres from "postgres";
-
 
 describe("execute", () => {
   const database = "dump_hook_ts_test";
@@ -14,58 +13,60 @@ describe("execute", () => {
     dumpHook = new DumpHook({
       database: database
     });
-  })
+  });
 
   afterEach(() => {
     execSync(`dropdb ${database}`);
     rmSync("tmp", { recursive: true });
-  })
+  });
 
   test("create default dumps location", async () => {
     expect(existsSync("tmp/dump_hook")).toBeFalsy();
-    await dumpHook!.execute("first", async ()=> {});
+    await dumpHook!.execute("first", async () => {});
     expect(existsSync("tmp/dump_hook")).toBeTruthy();
-  })
+  });
 
   test("create dump file", async () => {
     const filepath = "tmp/dump_hook/first.dump";
     expect(existsSync(filepath)).toBeFalsy();
-    await dumpHook!.execute("first", async ()=> {});
+    await dumpHook!.execute("first", async () => {});
     expect(existsSync(filepath)).toBeTruthy();
-  })
+  });
 
   test("create dump", async () => {
-    expect(async () => { await dumpHook!.execute("first_dump", async () => {})}).not.toThrow();
-  })
+    expect(async () => {
+      await dumpHook!.execute("first_dump", async () => {});
+    }).not.toThrow();
+  });
 
   describe("with data", () => {
     let sql: ReturnType<typeof postgres>;
 
     beforeEach(async () => {
-      sql = postgres({database: database});
-      await sql`create table t (a text, b text)`
-    })
+      sql = postgres({ database: database });
+      await sql`create table t (a text, b text)`;
+    });
 
     afterEach(async () => {
       await sql.end();
-    })
+    });
 
     test("fill from callback", async () => {
       await dumpHook.execute("with_data", async () => {
         await sql`insert into t values('a', 'b')`;
-      })
-      expect(await sql`select * from t`).toEqual([{a: 'a', b: 'b'}]);
-    })
+      });
+      expect(await sql`select * from t`).toEqual([{ a: "a", b: "b" }]);
+    });
 
     test("don't launch call back", async () => {
       await dumpHook.execute("with_data", async () => {
         await sql`insert into t values('a', 'b')`;
-      })
+      });
       await sql`delete from t`;
       expect(await sql`select * from t`).toEqual([]);
 
-      await dumpHook.execute("with_data", async () => {})
-      expect(await sql`select * from t`).toEqual([{a: 'a', b: 'b'}]);
-    })
-  })
-})
+      await dumpHook.execute("with_data", async () => {});
+      expect(await sql`select * from t`).toEqual([{ a: "a", b: "b" }]);
+    });
+  });
+});
